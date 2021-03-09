de858c67be6a4eb117346cb15f0ce6f81ae80177
Amanda Smith <amanda.smith@crometrics.com>
2021-03-09 14:34:12 -0600

    Moved Router into an outside provider, and made all modals close on navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
-import {BrowserRouter as Router, Switch, Route, useLocation} from "react-router-dom";
+import {Switch, Route, useLocation} from "react-router-dom";
 import {useDispatch} from "react-redux";
 import {useEffect} from "react";
 
 import {yarnSearchSubmitted} from "../slices/yarnsSlice";
 import {screenResized} from "../slices/uiSlice";
+import {allModalsClosed} from "../slices/modalsSlice";
 
 import "./App.scss";
 import HomePage from "./general//HomePage";
@@ -13,17 +14,9 @@ import TagPage from "./tags/TagPage";
 import Navbar from "./general/Navbar";
 import BackToTop from "./general/BackToTop";
 
-function AutoScrollToTopOnNav() {
-  const {pathname} = useLocation();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-}
-
 export default function App(props) {
   const dispatch = useDispatch();
+  const location = useLocation();
 
   // update screen size in store
   useEffect(() => {
@@ -33,7 +26,7 @@ export default function App(props) {
     return () => window.removeEventListener("resize", handleWindowResize);
   }, [dispatch]);
 
-  // wipe search query when leaving the page
+  // wipe search query when leaving the site
   // this is a substitute for using nested persist reducers to blacklist this part of the store, two layers deep
   // (this way we have just one storage entry to make backup/restore simple)
   useEffect(() => {
@@ -46,19 +39,23 @@ export default function App(props) {
     return () => window.removeEventListener("beforeunload", wipeSearch);
   });
 
+  // scroll to top and close all modals when route changes
+  // if more stuff gets added here, should probably refactor it into a single action that many reducers handle
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    dispatch(allModalsClosed());
+  }, [location, dispatch]);
+
   return (
-    <Router>
-      <AutoScrollToTopOnNav />
-      <div className="App">
-        <Navbar />
-      	<Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/yarns/:yarnID" component={YarnPage} />
-          <Route path="/yarns/:yarnID/colorways/:colorwayID" component={ColorwayPage} />
-          <Route path="/tags/:tagID" component={TagPage} />
-        </Switch>
-        <BackToTop />
-      </div>
-    </Router>
+    <div className="App">
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/yarns/:yarnID" component={YarnPage} />
+        <Route path="/yarns/:yarnID/colorways/:colorwayID" component={ColorwayPage} />
+        <Route path="/tags/:tagID" component={TagPage} />
+      </Switch>
+      <BackToTop />
+    </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter as Router} from "react-router-dom";
 import {Provider} from "react-redux";
 import {PersistGate} from "redux-persist/lib/integration/react";
 
@@ -11,7 +12,9 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
     	<PersistGate loading={null} persistor={persistor}>
-    		<App />
+    		<Router>
+    			<App />
+    		</Router>
     	</PersistGate>
     </Provider>
   </React.StrictMode>,
